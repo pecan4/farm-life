e4132d5c63969487939f14c1b2d982cc973e61df
pecan4 <157906054+pecan4@users.noreply.github.com>
2024-12-20 13:59:09 -0500

    trying to add more items

--- main.ts
+++ main.ts
@@ -1,8 +1,279 @@
+function place_tile_facing (mySprite: Sprite, myImage: Image, wall: boolean) {
+    if (characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.FacingUp))) {
+        tiles.setTileAt(mySprite.tilemapLocation().getNeighboringLocation(CollisionDirection.Top), myImage)
+        tiles.setWallAt(mySprite.tilemapLocation().getNeighboringLocation(CollisionDirection.Top), wall)
+    } else if (characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.FacingRight))) {
+        tiles.setTileAt(mySprite.tilemapLocation().getNeighboringLocation(CollisionDirection.Right), myImage)
+        tiles.setWallAt(mySprite.tilemapLocation().getNeighboringLocation(CollisionDirection.Right), wall)
+    } else if (characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.FacingDown))) {
+        tiles.setTileAt(mySprite.tilemapLocation().getNeighboringLocation(CollisionDirection.Bottom), myImage)
+        tiles.setWallAt(mySprite.tilemapLocation().getNeighboringLocation(CollisionDirection.Bottom), wall)
+    } else if (characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.FacingLeft))) {
+        tiles.setTileAt(mySprite.tilemapLocation().getNeighboringLocation(CollisionDirection.Left), myImage)
+        tiles.setWallAt(mySprite.tilemapLocation().getNeighboringLocation(CollisionDirection.Left), wall)
+    }
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (toolbar.get_items()[toolbar.get_number(ToolbarNumberAttribute.SelectedIndex)].get_text(ItemTextAttribute.Name) == "Hoe") {
-        tiles.setTileAt(mySprite.tilemapLocation(), assets.tile`myTile`)
+    if (!(toolbar.get_items()[toolbar.get_number(ToolbarNumberAttribute.SelectedIndex)].get_text(ItemTextAttribute.Name) == "")) {
+        if (toolbar.get_items()[toolbar.get_number(ToolbarNumberAttribute.SelectedIndex)].get_text(ItemTextAttribute.Name) == "Hoe") {
+            place_tile_facing(mySprite, assets.tile`myTile`, false)
+        }
     }
 })
+function character_animation () {
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f . . . . . . . . . 
+        . . . . . f . . . . . . . . . . 
+        . . . . f . . . . . . . . . . . 
+        . . . f . . . . . . . . . . . . 
+        . . f . . . . . . . . . . . . . 
+        . f . . . . . . . . . . . . . . 
+        f f f f f f f f f f f f f f f f 
+        . f . . . . . . . . . . . . . . 
+        . . f . . . . . . . . . . . . . 
+        . . . f . . . . . . . . . . . . 
+        . . . . f . . . . . . . . . . . 
+        . . . . . f . . . . . . . . . . 
+        . . . . . . f . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.MovingLeft)
+    )
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f . . . . . . . . . 
+        . . . . . f . . . . . . . . . . 
+        . . . . f . . . . . . . . . . . 
+        . . . f . . . . . . . . . . . . 
+        . . f . . . . . . . . . . . . . 
+        . f . . . . . . . . . . . . . . 
+        f f f f f f f f f f f f f f f f 
+        . f . . . . . . . . . . . . . . 
+        . . f . . . . . . . . . . . . . 
+        . . . f . . . . . . . . . . . . 
+        . . . . f . . . . . . . . . . . 
+        . . . . . f . . . . . . . . . . 
+        . . . . . . f . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.FacingLeft)
+    )
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . f . . . . . f . . . . . f . . 
+        . . f . . . . f . . . . f . . . 
+        . . . f . . . f . . . f . . . . 
+        . . . . f . . f . . f . . . . . 
+        . . . . . f . f . f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.MovingDown)
+    )
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . f . . . . . f . . . . . f . . 
+        . . f . . . . f . . . . f . . . 
+        . . . f . . . f . . . f . . . . 
+        . . . . f . . f . . f . . . . . 
+        . . . . . f . f . f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.FacingDown)
+    )
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . f . . . . . 
+        . . . . . . . . . . . f . . . . 
+        . . . . . . . . . . . . f . . . 
+        . . . . . . . . . . . . . f . . 
+        . . . . . . . . . . . . . . f . 
+        f f f f f f f f f f f f f f f f 
+        . . . . . . . . . . . . . . f . 
+        . . . . . . . . . . . . . f . . 
+        . . . . . . . . . . . . f . . . 
+        . . . . . . . . . . . f . . . . 
+        . . . . . . . . . . f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.MovingRight)
+    )
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . f . . . . . 
+        . . . . . . . . . . . f . . . . 
+        . . . . . . . . . . . . f . . . 
+        . . . . . . . . . . . . . f . . 
+        . . . . . . . . . . . . . . f . 
+        f f f f f f f f f f f f f f f f 
+        . . . . . . . . . . . . . . f . 
+        . . . . . . . . . . . . . f . . 
+        . . . . . . . . . . . . f . . . 
+        . . . . . . . . . . . f . . . . 
+        . . . . . . . . . . f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.FacingRight)
+    )
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . . f . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . f . f . f . . . . . . 
+        . . . . f . . f . . f . . . . . 
+        . . . f . . . f . . . f . . . . 
+        . . f . . . . f . . . . f . . . 
+        . f . . . . . f . . . . . f . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.MovingUp)
+    )
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . . f . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . f . f . f . . . . . . 
+        . . . . f . . f . . f . . . . . 
+        . . . f . . . f . . . f . . . . 
+        . . f . . . . f . . . . f . . . 
+        . f . . . . . f . . . . . f . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.FacingUp)
+    )
+}
+controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (inventory_open) {
+        inventory_open = false
+    } else if (!(inventory_open)) {
+        inventory_open = true
+    }
+})
+function create_all_items_array () {
+    all_items = [
+    Inventory.create_item("Hoe", img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f f f f . . 
+        . . . . f f f f f f f f 7 f . . 
+        . . . . f f f f f f f f f f . . 
+        . . . . . . . . . . 7 7 7 . . . 
+        . . . . . . . . . 7 7 7 . . . . 
+        . . . . . . . . 7 7 7 . . . . . 
+        . . . . . . . 7 7 7 . . . . . . 
+        . . . . . . 7 7 7 . . . . . . . 
+        . . . . . 7 7 7 . . . . . . . . 
+        . . . . 7 7 7 . . . . . . . . . 
+        . . . 7 7 7 . . . . . . . . . . 
+        . . 7 7 7 . . . . . . . . . . . 
+        . 7 7 7 . . . . . . . . . . . . 
+        . . 7 . . . . . . . . . . . . . 
+        `, "Use this tool to till soil."),
+    Inventory.create_item("Carrot Seeds", assets.image`carrot seeds`, "Plant these in tilled soil to produce carrots! Takes 3 days to grow"),
+    Inventory.create_item("", img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `),
+    Inventory.create_item("", img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    ]
+}
+let inventory_open = false
+let all_items: Inventory.Item[] = []
 let toolbar: Inventory.Toolbar = null
 let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level3`)
@@ -26,124 +297,49 @@ mySprite = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
-characterAnimations.loopFrames(
-mySprite,
-[img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . f . . . . . . . . . 
-    . . . . . f . . . . . . . . . . 
-    . . . . f . . . . . . . . . . . 
-    . . . f . . . . . . . . . . . . 
-    . . f . . . . . . . . . . . . . 
-    . f . . . . . . . . . . . . . . 
-    f f f f f f f f f f f f f f f f 
-    . f . . . . . . . . . . . . . . 
-    . . f . . . . . . . . . . . . . 
-    . . . f . . . . . . . . . . . . 
-    . . . . f . . . . . . . . . . . 
-    . . . . . f . . . . . . . . . . 
-    . . . . . . f . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `],
-500,
-characterAnimations.rule(Predicate.MovingLeft)
-)
-characterAnimations.loopFrames(
-mySprite,
-[img`
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . f . . . . . f . . . . . f . . 
-    . . f . . . . f . . . . f . . . 
-    . . . f . . . f . . . f . . . . 
-    . . . . f . . f . . f . . . . . 
-    . . . . . f . f . f . . . . . . 
-    . . . . . . f f f . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    `],
-500,
-characterAnimations.rule(Predicate.MovingDown)
-)
-characterAnimations.loopFrames(
-mySprite,
-[img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . f . . . . . 
-    . . . . . . . . . . . f . . . . 
-    . . . . . . . . . . . . f . . . 
-    . . . . . . . . . . . . . f . . 
-    . . . . . . . . . . . . . . f . 
-    f f f f f f f f f f f f f f f f 
-    . . . . . . . . . . . . . . f . 
-    . . . . . . . . . . . . . f . . 
-    . . . . . . . . . . . . f . . . 
-    . . . . . . . . . . . f . . . . 
-    . . . . . . . . . . f . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `],
-500,
-characterAnimations.rule(Predicate.MovingRight)
-)
-characterAnimations.loopFrames(
-mySprite,
-[img`
-    . . . . . . . f . . . . . . . . 
-    . . . . . . f f f . . . . . . . 
-    . . . . . f . f . f . . . . . . 
-    . . . . f . . f . . f . . . . . 
-    . . . f . . . f . . . f . . . . 
-    . . f . . . . f . . . . f . . . 
-    . f . . . . . f . . . . . f . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    . . . . . . . f . . . . . . . . 
-    `],
-500,
-characterAnimations.rule(Predicate.MovingUp)
-)
+character_animation()
 toolbar = Inventory.create_toolbar([], 7)
 toolbar.set_color(ToolbarColorAttribute.BoxOutline, 15)
-toolbar.set_color(ToolbarColorAttribute.BoxSelectedOutline, 13)
+toolbar.set_color(ToolbarColorAttribute.BoxSelectedOutline, 8)
 toolbar.set_color(ToolbarColorAttribute.BoxBackground, 1)
 toolbar.set_color(ToolbarColorAttribute.BoxText, 15)
 toolbar.setFlag(SpriteFlag.RelativeToCamera, true)
 toolbar.setPosition(80, 108)
-let hoe_item = Inventory.create_item("Hoe", img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . f f f f f f f f f f . . 
-    . . . . f f f f f f f f 7 f . . 
-    . . . . f f f f f f f f f f . . 
-    . . . . . . . . . . 7 7 7 . . . 
-    . . . . . . . . . 7 7 7 . . . . 
-    . . . . . . . . 7 7 7 . . . . . 
-    . . . . . . . 7 7 7 . . . . . . 
-    . . . . . . 7 7 7 . . . . . . . 
-    . . . . . 7 7 7 . . . . . . . . 
-    . . . . 7 7 7 . . . . . . . . . 
-    . . . 7 7 7 . . . . . . . . . . 
-    . . 7 7 7 . . . . . . . . . . . 
-    . 7 7 7 . . . . . . . . . . . . 
-    . . 7 . . . . . . . . . . . . . 
-    `, "Use this tool to till soil.")
-let toolbar_items = [hoe_item]
+create_all_items_array()
+let toolbar_items = [all_items[0]]
+let inventory = Inventory.create_inventory([], 32)
+inventory.set_color(InventoryColorAttribute.InventoryOutline, 15)
+inventory.set_color(InventoryColorAttribute.InventorySelectedOutline, 8)
+inventory.set_color(InventoryColorAttribute.InventoryBackground, 1)
+inventory.set_color(InventoryColorAttribute.InventoryText, 15)
+inventory.setFlag(SpriteFlag.RelativeToCamera, true)
+inventory.left = 4
+inventory.top = 4
+let inventory_items = [all_items[1]]
+inventory_open = false
+forever(function () {
+    if (inventory_open) {
+        if (controller.B.isPressed()) {
+            controller.moveSprite(mySprite, 0, 0)
+            if (controller.left.isPressed()) {
+                toolbar.change_number(ToolbarNumberAttribute.SelectedIndex, -1)
+                pauseUntil(() => !(controller.left.isPressed()))
+            } else if (controller.right.isPressed()) {
+                toolbar.change_number(ToolbarNumberAttribute.SelectedIndex, 1)
+                pauseUntil(() => !(controller.right.isPressed()))
+            }
+            if (toolbar.get_number(ToolbarNumberAttribute.SelectedIndex) > 6) {
+                toolbar.set_number(ToolbarNumberAttribute.SelectedIndex, 0)
+            } else if (toolbar.get_number(ToolbarNumberAttribute.SelectedIndex) < 0) {
+                toolbar.set_number(ToolbarNumberAttribute.SelectedIndex, 6)
+            }
+        } else {
+            controller.moveSprite(mySprite, 100, 100)
+        }
+    }
+})
 forever(function () {
     toolbar.set_items(toolbar_items)
+    inventory.set_items(inventory_items)
+    inventory.setFlag(SpriteFlag.Invisible, inventory_open)
 })
